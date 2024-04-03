37124ba5f8a9f8bc825bae6249c2da1e8076054d
raghav1482 <raghvendra1482002@gmail.com>
2024-04-03 22:21:22 +0530

    error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,11 @@ import TopContain from "../components/TopContain";
 import Sliderr from "../components/Slider";
 import { useState } from "react";
 export default function Home() {
-  const [night , setNight]=useState(true);
-  const handleNight = ()=>{
-    setNight(!night);
-  }
   return (
     <main className="main flex min-h-screen flex-col items-center justify-between">
       <h3 className="main-h3">Sports</h3>
-      <button onClick={()=>{handleNight();}}>night</button>
-      <TopContain  nmode={night}/>
-      <Sliderr  nmode={night}/>
+      <TopContain/>
+      <Sliderr/>
     </main>
   );
 }
